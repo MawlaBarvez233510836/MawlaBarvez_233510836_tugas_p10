a1736e8926661aa0c5d3a4acaeffaa458d9e3bf7
MawlaBarvez233510836 <mawlabarvez@student.uir.ac.id>
2025-05-25 23:48:40 +0800

    pembatlan perubahan

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/pbk.p10/',
-  plugins: [vue()]
+  plugins: [vue()],
 })
